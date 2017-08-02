bc3b2d6973baf68cdcac600e542b6c46e5129af9
Nishat <nishat.eee08@gmail.com>
2017-08-02 17:01:42 -0500

    added files

---
+++ get_plots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ~/research/videonav/plots/cdf_time.pdf plots/
+cp ~/research/videonav/plots/walk_cdf_time.pdf plots/
+cp ~/research/videonav/systemdiagram.pdf figures/
+cp ~/research/videonav/plots/rec_filter.jpg plots/
+cp ~/research/videonav/plots/norec_filter.jpg plots/
+cp ~/research/videonav/plots/sysdia.png figures/
+cp ~/research/videonav/plots/rec_mv.jpg plots/
+cp ~/research/videonav/plots/rec_mv1.jpg plots/
+cp ~/research/videonav/plots/rec_mv2.jpg plots/
+cp ~/research/videonav/plots/rec_enl.jpg plots/
+cp ~/research/videonav/plots/rec_enl1.jpg plots/
+
