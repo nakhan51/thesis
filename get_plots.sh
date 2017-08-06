c3dd955a507351f65b48ee07b6984da6167ba377
Nishat <nishat.eee08@gmail.com>
2017-08-06 15:10:25 -0500

    modified background

--- get_plots.sh
+++ get_plots.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-cp ~/research/videonav/plots/cdf_time.pdf plots/
-cp ~/research/videonav/plots/walk_cdf_time.pdf plots/
-cp ~/research/videonav/systemdiagram.pdf figures/
-cp ~/research/videonav/plots/rec_filter.jpg images/
-cp ~/research/videonav/plots/norec_filter.jpg images/
-cp ~/research/videonav/plots/sysdia.png figures/
-cp ~/research/videonav/plots/rec_mv.jpg images/
-cp ~/research/videonav/plots/rec_mv1.jpg images/
-cp ~/research/videonav/plots/rec_mv2.jpg images/
-cp ~/research/videonav/plots/rec_enl.jpg images/
-cp ~/research/videonav/plots/rec_enl1.jpg images/
-cp ~/research/videonav/plots/Detectedgreencircles.jpg images/
-cp ~/research/videonav/plots/Detectedredcircles.jpg images/
-cp ~/research/videonav/plots/RedGreenfiltering.jpg images/
-cp ~/research/videonav/plots/RedGreenfiltering_red.jpg images/
-cp ~/research/videonav/plots/frame301.jpg images/
-cp ~/research/videonav/plots/frame502.jpg images/
+#cp ~/research/videonav/plots/cdf_time.pdf plots/
+#cp ~/research/videonav/plots/walk_cdf_time.pdf plots/
+cp ~/research/videonav/sysdia.pdf figures/
+
+
+#cp ~/research/videonav/plots/rec_filter.jpg images/
+#cp ~/research/videonav/plots/norec_filter.jpg images/
+#cp ~/research/videonav/plots/rec_mv.jpg images/
+#cp ~/research/videonav/plots/rec_mv1.jpg images/
+#cp ~/research/videonav/plots/rec_mv2.jpg images/
+#cp ~/research/videonav/plots/rec_enl.jpg images/
+#cp ~/research/videonav/plots/rec_enl1.jpg images/
+#cp ~/research/videonav/plots/Detectedgreencircles.jpg images/
+#cp ~/research/videonav/plots/Detectedredcircles.jpg images/
+#cp ~/research/videonav/plots/RedGreenfiltering.jpg images/
+#cp ~/research/videonav/plots/RedGreenfiltering_red.jpg images/
+#cp ~/research/videonav/plots/frame301.jpg images/
+#cp ~/research/videonav/plots/frame502.jpg images/
